4677d582becd5a8e6d2317e898b599f73c353163
Lenore Watson <lenorewatson@gmail.com>
2023-02-13 09:16:25 -0600

    deleted models and migrations

--- EFCoreApp/EFCoreApp/Data/ApplicationDbContext.cs
+++ EFCoreApp/EFCoreApp/Data/ApplicationDbContext.cs
@@ -13,21 +13,15 @@ namespace EFCoreApp.Data
         {
         }
 
-        public DbSet<Address> Addresses { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
-        public DbSet<City> Cities { get; set; }
-        public DbSet<Country> Countries { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<AppUser> AppUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Address>().ToTable(nameof(Address));
+;
             modelBuilder.Entity<Appointment>().ToTable(nameof(Appointment));
-            modelBuilder.Entity<City>().ToTable(nameof(City));
-            modelBuilder.Entity<Country>().ToTable(nameof(Country));
             modelBuilder.Entity<Customer>().ToTable(nameof(Customer));
             modelBuilder.Entity<AppUser>().ToTable(nameof(AppUser));
 
